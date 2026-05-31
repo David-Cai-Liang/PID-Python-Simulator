665a0a11b3cbd0b89a84cf3c0a43c21582334db1
David Cai Liang <dac828@lehigh.edu>
2026-05-31 16:20:56 -0400

    initial pid

--- pid.py
+++ pid.py
@@ -1,20 +1,20 @@
-import numpy as np 
-import matplotlib 
-import turtle 
-import time 
+import numpy as np
+import matplotlib
+import turtle
+import time
 
-#GLOBAL PARAMS 
-TIMER = 0 
-SETPOINT = 10 #final goal 
+#GLOBAL PARAMS
+TIMER = 0
+SETPOINT = 10 #final goal
 SIM_TIME = 100    # in sec
 
 TIME_STEP = 0.005
 
 #_______
-INITIAL_X = 0 
+INITIAL_X = 0
 INITIAL_Y = -100
 MASS = 1 #kg
-MAX_THRUST = 15 #Newtons 
+MAX_THRUST = 15 #Newtons
 g = -9.81 # Gravitational constant
 
 V_i = 0 #initial velocity
@@ -23,16 +23,16 @@ Y_i = 0 #initial height
 
 # -----------
 
-class Simulation(object): 
-    def __init__(self):
+class Simulation(object):
+    def __init__(self,KP=0.025,KI=0,KD=0.0125):
 
         self.Insight = Rocket()
 
         self.screen = turtle.Screen()
         self.screen.setup(1280, 900)
-        self.marker = turtle.Turtle() 
+        self.marker = turtle.Turtle()
 
-        #Goal we are getting to (setpoint/ marker)
+        #Move marker to set_point
         self.marker.penup()
         self.marker.left(180)
         self.marker.goto(15,SETPOINT)
@@ -40,37 +40,38 @@ class Simulation(object):
         self.sim = True
         self.timer = 0
 
-    #do our simulation cycles?
-    def cycle(self): 
-        while(self.sim): 
-        
-            #get a thrust output from our PID
-            thrust = 10 #newtons
+        # Proportional Control
+        self.pid = PID(KP,KI,KD,SETPOINT)
+
+    # simulation cycles
+    # give access to pid implementation
+    def cycle(self):
+        while(self.sim):
+
+            # generate thrust output using PID
+            thrust = min(MAX_THRUST,self.pid.compute(self.Insight.get_y()))
+
             self.Insight.set_ddy(thrust)
             self.Insight.set_dy()
             self.Insight.set_y()
             time.sleep(TIME_STEP)
-            self.timer +=1 
+            self.timer +=1
 
-            if self.timer > SIM_TIME: 
-                self.sim = False
-            
-            elif self.Insight.get_y() > 800: 
-                self.sim = False
-            
-            elif self.Insight.get_y() < -800: 
+            if self.timer > SIM_TIME:
                 self.sim = False
 
+            elif self.Insight.get_y() > 800:
+                self.sim = False
 
+            elif self.Insight.get_y() < -800:
+                self.sim = False
 
+        if (self.timer > SIM_TIME):
+            print("Test Success")
+            print("Error:",self.pid.get_abs_integral_error())
+        else:
+            print("Test Failure")
 
-            
-    #1 degree of freedom problem 2 sep PID problem
-
-
-#thrust verc control rocket, Integral builds over time, how far away for how long
-#-->Gimple for 3 sec only 3 sec for integral error, PD controler 
-# misalignment, (Slowly correct for upright minimize translational motion)
 class Rocket(object):
     def __init__(self):
         global Rocket
@@ -92,11 +93,11 @@ class Rocket(object):
         self.ddy = g + thrust/MASS # thrust impact system ==> output of PID controller
 
     def get_ddy(self):
-        return self.ddy 
+        return self.ddy
 
     def set_dy(self):
         self.dy += self.ddy #2nd derivate ddy has it's own Intergral this is  Velocity
-   
+
     def get_dy(self):
         return self.dy
 
@@ -111,27 +112,39 @@ class Rocket(object):
         return self.y
 
 
-class PID(object): 
-    def __init__(self, KP, KI, KD, target): 
+class PID(object):
+    def __init__(self, KP, KI, KD, target):
         self.kp = KP
-        self.ki = KI 
+        self.ki = KI
         self.kd = KD # quickly adjust for subpoint swing or overshoot subpoint
-        self.setpoint = target 
+        self.set_point = target
         self.error = 0
         self.integral_error = 0
-        
+        self.abs_integral_error = 0
+
         self.error_last = 0
-        self.derivative_error = 0 
+        self.derivative_error = 0
         self.output = 0
-    def computer(self, pos):
-        self.error = target - pos 
-        self. integra
+
+    def compute(self, pos):
+        self.error_last = self.error
+        self.error = self.set_point - pos;
+        self.derivative_error = (self.error - self.error_last) / TIME_STEP
+        self.integral_error += self.error
+        self.abs_integral_error += abs(self.error)
+        self.output = self.kp * self.error + self.ki * self.integral_error + self.kd * self.derivative_error
+        # print("error:",self.error)
+        # print("integral_error:",self.integral_error)
+        # print("derivative_error:",self.derivative_error)
+        # print("Output:",self.output)
+        return self.output
+
+    def get_abs_integral_error(self):
+        return self.abs_integral_error
 
 def main():
-    # while(TIMER < 5):
+
     sim = Simulation()
-    #     time.sleep(1)
-    #     timer +=1
     sim.cycle()
 
 main()
